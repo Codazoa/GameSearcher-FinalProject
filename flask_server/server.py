b22462669ecab3215159cd9da46b55470640a1af
Cody Vernon <cody.vernon@wsu.edu>
2022-04-13 13:15:09 -0700

    update .gitignore and server.py work

--- flask_server/server.py
+++ flask_server/server.py
@@ -1,19 +1,27 @@
 import classes
 from flask import Flask, render_template, url_for, request
+from flask_cors import CORS
 import whoosh
 from whoosh.index import open_dir
 from whoosh.fields import *
 from whoosh.qparser import QueryParser
 from whoosh.qparser import MultifieldParser
 from whoosh import qparser
+import json
 
 # flask server for backend
 
 app = Flask(__name__)
+CORS(app)
 
-@app.route('/search')
+@app.route('/')
+def home():
+    return ('Hello world')
+
+@app.route('/gameresults', methods=['POST', 'GET'])
 def search():
-    return {'test': 123}
+    return "No search given"
+
 
 if __name__ == '__main__':
     app.run(debug=True)
